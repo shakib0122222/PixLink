bde1c5ee1d759ae796a9382710df5f733054b6f7
My Information <thelofi07@gmail.com>
2025-12-10 12:35:41 +0600

    Create firebase.js

---
+++ js/firebase.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDpv3NaTYjrmeoN19itDACcvEpMggd72H8",
+  authDomain: "url-c576f.firebaseapp.com",
+  projectId: "url-c576f",
+  storageBucket: "url-c576f.firebasestorage.app",
+  messagingSenderId: "681639648878",
+  appId: "1:681639648878:web:31d36b7e0c8031c806d07f",
+  measurementId: "G-HKS96B2T7T"
+};
+
+firebase.initializeApp(firebaseConfig);
